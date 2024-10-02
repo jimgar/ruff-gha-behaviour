55d30e7a7f6ebbb6c70553f19b74a33184bff3d7
jimgar <jimgar@users.noreply.github.com>
2024-10-02 16:33:43 +0000

    Style code (GHA)

--- rings.py
+++ rings.py
@@ -1,13 +1,28 @@
 import os
 import random
 
-CHARACTERS = ("Frodo", "Sam", "Merry", "Pippin", "Aragorn", "Legolas", "Gimli", "Boromir", "Gandalf", "Saruman", "Sauron")
+CHARACTERS = (
+    "Frodo",
+    "Sam",
+    "Merry",
+    "Pippin",
+    "Aragorn",
+    "Legolas",
+    "Gimli",
+    "Boromir",
+    "Gandalf",
+    "Saruman",
+    "Sauron",
+)
+
 
 def random_character():
     return random.choice(CHARACTERS)
 
+
 def ring_bearer():
     return name in ("Frodo", "Sam")
+
 
 if __name__ == "__main__":
     character = random_character()
